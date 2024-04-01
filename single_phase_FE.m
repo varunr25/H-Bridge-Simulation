542f6ea81035da2c573f36a594c93557ef1d9555
varunr25 <varunjunk25@gmail.com>
2024-04-01 16:25:03 -0400

    moved calcs to bottom; still not working

--- single_phase_FE.m
+++ single_phase_FE.m
@@ -3,83 +3,74 @@
 % single_phase_FE.m
 
 while t_vec(k) < tend
-    V_as(k) = V_m * cos(omega * t_vec(k));  % AC voltage 
-    dI_dt = (V_as(k) - R * i_AC(k)) / L;    % Change in current
-    i_AC(k+1) = i_AC(k) + dI_dt * dt;       % AC current for next step
+    V_as(k) = V_m * cos(omega * t_vec(k));  % AC voltage
 
     if V_as(k) >= 0     
     % SW1 and SW4 are on, SW2 and SW3 are off during positive half-cycle
+        switch_bool = 1;
+        
         % SW1 conducts
-        V_T1(k) = 0; 
-        i_T1(k) = i_AC(k); 
+        V_T1(k) = 0;            i_T1(k) = i_AC(k); 
         
         % SW4 conducts
-        V_T4(k) = 0; 
-        i_T4(k) = i_AC(k);
+        V_T4(k) = 0;            i_T4(k) = i_AC(k);
         
         % SW2 is off
-        V_T2(k) = V_DC; 
-        i_T2(k) = 0;
+        V_T2(k) = V_DC;         i_T2(k) = 0;
         
         % SW3 is off
-        V_T3(k) = V_DC; 
-        i_T3(k) = 0;
+        V_T3(k) = V_DC;         i_T3(k) = 0;
 
     % Diodes conduct when corresponding switches are off
         % D2 conducts
-        V_D2(k) = 0; 
-        i_D2(k) = i_AC(k); 
+        V_D2(k) = 0;            i_D2(k) = i_AC(k); 
 
         % D3 conducts        
-        V_D3(k) = 0; 
-        i_D3(k) = i_AC(k); 
+        V_D3(k) = 0;            i_D3(k) = i_AC(k); 
 
         % D1 is off
-        V_D1(k) = V_DC; 
-        i_D1(k) = 0; 
+        V_D1(k) = V_DC;         i_D1(k) = 0; 
 
         % D4 is off
-        V_D4(k) = V_DC; 
-        i_D4(k) = 0; 
+        V_D4(k) = V_DC;         i_D4(k) = 0; 
 
-    else    
+    else   
     % SW2 and SW3 are on, SW1 and SW4 are off during negative half-cycle
+        switch_bool = 0;
+            
         % SW2 conducts
-        V_T2(k) = 0; 
-        i_T2(k) = i_AC(k); 
+        V_T2(k) = 0;            i_T2(k) = i_AC(k); 
         
         % SW3 conducts
-        V_T3(k) = 0; 
-        i_T3(k) = i_AC(k); 
+        V_T3(k) = 0;            i_T3(k) = i_AC(k); 
         
         % SW1 is off 
-        V_T1(k) = V_DC; 
-        i_T1(k) = 0;
+        V_T1(k) = V_DC;         i_T1(k) = 0;
         
         % SW4 is off
-        V_T4(k) = V_DC; 
-        i_T4(k) = 0;
+        V_T4(k) = V_DC;         i_T4(k) = 0;
         
     % Diodes conduct when corresponding switches are off
         % D1 conducts
-        V_D1(k) = 0; 
-        i_D1(k) = i_AC(k); 
+        V_D1(k) = 0;            i_D1(k) = i_AC(k); 
         
         % D4 conducts
-        V_D4(k) = 0; 
-        i_D4(k) = i_AC(k); 
+        V_D4(k) = 0;            i_D4(k) = i_AC(k); 
         
         % D2 is off
-        V_D2(k) = V_DC; 
-        i_D2(k) = 0; 
+        V_D2(k) = V_DC;         i_D2(k) = 0; 
         
         % D3 is off
-        V_D3(k) = V_DC; 
-        i_D3(k) = 0; 
+        V_D3(k) = V_DC;         i_D3(k) = 0; 
     end
+ 
+    V_AC(k) = ((2 * switch_bool) - 1) * V_DC;
+    dI_dt(k) = (V_AC(k) - (R * i_AC(k))) / L;     % Change in current
+    i_AC(k + 1) = i_AC(k) + dI_dt(k) * dt;       % AC current for next step
+
     theta_ac_rad(k) = omega * t_vec(k);             % theta_ac in radians
     theta_ac_deg(k) = rad2deg(theta_ac_rad(k));     % theta_ac in degrees
-
+    
     t_vec(k + 1) = t_vec(k) + dt;   
     k = k + 1;
 end
